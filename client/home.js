0d346a04b28b5cb7a002f0f8c2e0d82c093171f9
neobii <iwilldevelopcodeforyou@gmail.com>
2016-08-28 00:35:24 -0500

    added random directive

--- client/home.js
+++ client/home.js
@@ -1,5 +1,9 @@
 Template.homeView.helpers({
-	directiveTypes(){
-		return DirectiveTypes.find();
+	randomPrompts(){
+		let dirArray = [];
+		for(let i=0;i<20;i++) {
+			dirArray.push(App.getRandomDirective());
+		}
+		return dirArray;
 	}
 })
